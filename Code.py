2de31a7c81c08259a3977392358c60f843f5e0fa
Bradley Pelton <bradleypelton@gmail.com>
2018-12-12 17:31:22 -0500

    adding additional tests and the backpropagation
    A large deal of credit is due to the author of the following link http://iamtrask.github.io/2015/07/12/basic-python-network/
    for the base code of the backpropagation

--- Code.py
+++ Code.py
@@ -8,15 +8,50 @@ def sigmoid_prime(x):
     """ evaluating the derivate of sigmoid at x"""
     return x*(1-x)
 
-#input data 
+#input data (10 numbers)
 
-test_1= [1,1,1,1,1,1,1,1,1,1]
-test_2= [0,0,0,0,0,0,0,0,0,0]
-test_3= [1,0,1,0,1,0,1,0,1,0]
-test_4= [1,1,0,0,1,1,0,0,1,1]
-test_5= [0,0,1,1,0,0,1,1,0,0]
+test_1= [1,1,1,1,1,1,1,1,1,1]   #answer =1
+test_2= [0,0,0,0,0,0,0,0,0,0]   #answer =0 
+test_3= [1,0,1,0,1,0,1,0,1,0]   #answer =1
+test_4= [1,1,0,0,1,1,0,0,1,1]   #answer =0
+test_5= [0,0,1,1,0,0,1,1,0,0]   #answer =1
+test_6= [0,1,0,1,0,1,0,1,0,1]   #answer =0
+test_7= [1,1,0,1,0,1,0,1,0,1]   #answer =0
+test_8= [0,0,1,0,1,0,1,0,1,0]   #answer =1
+test_9= [0,0,0,1,1,1,0,0,0,1]   #answer =1
+test_10= [1,1,1,0,0,0,1,1,1,0]  #answer =0
 
-x= np.array
+#output data (11th number that the computer guesses)
 
-for i in range(10000):
+y = np.array([[1,0,1,0,1,0,0,1,1,0]]).T
+print(y)
+
+x= np.array([ test_1,test_2,test_3,test_4,test_5,
+              test_6,test_7,test_8,test_9,test_10 ] )
+print(x)
+
+np.random.seed(2222)
+
+weights = np.random.random((10,1))
+
+
+for i in range(100):
+    var_a= x
+    var_b= sigmoid(np.dot(var_a,weights))
+    var_b_error= y - var_b
+    var_b_delta = var_b_error * sigmoid_prime(var_b)
+    weights += np.dot(var_a.T,var_b_delta)
     
+
+print("output after training:")
+print(var_b)
+
+your_test= []
+while len(your_test) < 10:
+    your_number = int(input("Please enter either 0 or 1: "))
+    your_test.extend(your_number)
+    print(your_test)
+
+x= np.array([ test_1,test_2,test_3,test_4,test_5,
+              test_6,test_7,test_8,test_9,test_10,your_test ] )
+
