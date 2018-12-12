e4f7b4cb3bd490669e67c9cd993a9145541077fc
Bradley Pelton <bradleypelton@gmail.com>
2018-12-11 22:21:48 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,3 +7,16 @@ def sigmoid(x):
 def sigmoid_prime(x):
     """ evaluating the derivate of sigmoid at x"""
     return x*(1-x)
+
+#input data 
+
+test_1= [1,1,1,1,1,1,1,1,1,1]
+test_2= [0,0,0,0,0,0,0,0,0,0]
+test_3= [1,0,1,0,1,0,1,0,1,0]
+test_4= [1,1,0,0,1,1,0,0,1,1]
+test_5= [0,0,1,1,0,0,1,1,0,0]
+
+x= np.array
+
+for i in range(10000):
+    
